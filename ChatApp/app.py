aeff92f6e89a3c5c76d52e128c28f059e023c4d2
raguchi66 <hiraguchi.natsu0606@gmail.com>
2024-07-14 21:26:52 +0900

    registration関連のページ追加

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -4,14 +4,25 @@ from flask import Flask, render_template
 # Flaskアプリケーションのインスタンスを作成
 app = Flask(__name__)
 
+# cssのページの表示
+@app.route('/')
+def test():
+    return render_template('sample.html')
+
 # ログインページの表示
 @app.route('/login')
 def login():
     return render_template('registration/login.html')
 
-@app.route('/')
-def test():
-    return render_template('sample.html')
+# 新規会員登録ページの表示
+@app.route('/signup')
+def signup():
+    return render_template('registration/signup.html')
+
+# ホーム画面ページの表示
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 # アプリケーションを実行。ホストを'0.0.0.0'に設定し、ポート5001でリッスン。デバッグモードを有効にする
 if __name__ == "__main__":
