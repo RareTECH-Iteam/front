cbe5f93ddb0eb0a3b732858fa6dabcffe154b821
raguchi66 <hiraguchi.natsu0606@gmail.com>
2024-07-26 23:54:20 +0900

    プロフィール画面・チャット画面類の追加

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -24,6 +24,26 @@ def signup():
 def home():
     return render_template('home.html')
 
+# チャット一覧画面ページの表示
+@app.route('/chat_list')
+def chat_list():
+    return render_template('chat_list.html')
+
+# チャット画面ページの表示
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+# プロフィール画面ページの表示
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+# プロフィール編集画面ページの表示
+@app.route('/profile_edit')
+def profile_edit():
+    return render_template('profile_edit.html')
+
 # アプリケーションを実行。ホストを'0.0.0.0'に設定し、ポート5001でリッスン。デバッグモードを有効にする
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
